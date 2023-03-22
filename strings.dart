70bd44a6bd361ece0880c04136a23205904985e9
RhysRoo <rhys.bundy97@gmail.com>
2023-03-22 10:17:47 +0000

    Completed question 3

--- strings.dart
+++ strings.dart
@@ -12,21 +12,38 @@ double getArea(double radius) {
   return pi * toPower(radius);
 }
 
-personalGreeting(String name) {
+String personalGreeting(String name) {
   return 'Hey $name!';
+}
+
+String gradeTest(int score) {
+  String grade = '';
+  if (score >= 8) {
+    grade = 'A';
+  } else if (score >= 6) {
+    grade = 'B';
+  } else if (score >= 4) {
+    grade = 'C';
+  } else if (score >= 0) {
+    grade = 'F';
+  }
+  return grade;
 }
 
 void main() {
   // String myMessage = personalGreeting('Rhys');
   // print(myMessage);
 
-  //   double circumference = circumferenceOfCircle(10);
-  //   double area = getArea(10);
-  //   print(area);
-  //   print(circumference);
+  // double circumference = circumferenceOfCircle(10);
+  // double area = getArea(10);
+  // print(area);
+  // print(circumference);
 
-  String name = formalName('Rhys', 'Parsons');
-  print(name);
+  // String name = formalName('Rhys', 'Parsons');
+  // print(name);
+
+  String grade = gradeTest(7);
+  print(grade);
 }
 
 formalName(String firstName, String lastName) {
